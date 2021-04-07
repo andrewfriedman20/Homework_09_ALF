10ff76176cfd196cc750984827cb1c974dedb94e
Andrew Friedman <77944651+andrewfriedman20@users.noreply.github.com>
2021-04-06 22:47:28 -0500

    Final update generate mkdwn

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,5 +1,7 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
+'[Title](#title)',
+//  Function creates licenxe badge based on the user selection of a license badge
+// should the user not choose a license in the terminal, the field shall remain blank
+// 
 function renderLicenseBadge(license) {
   if (license === 'MIT') {
     "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)" 
@@ -13,49 +15,65 @@ function renderLicenseBadge(license) {
 
 }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
+// This returns the license link (or empty sting if user does not select a license)
 function renderLicenseLink(license) {
+  if (license === 'MIT') {
+    "[![License: MIT](https://opensource.org/licenses/MIT)]" 
+  }
+  else if (license === 'GPL') {
+    "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0),"
+  } else {
+    "[![License: IPL 1.0](https://img.shields.io/badge/License-IPL%201.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)"    
+  }
 
 
 }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
+// This creates the license section of the README (or, if user fails to select one, an empty string)
+
 function renderLicenseSection(license) {
-
+  if (license === 'MIT') {
+    "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)" 
+  }
+  else if (license === 'GPL') {
+    "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0),"
+  } else {
+    "[![License: IPL 1.0](https://img.shields.io/badge/License-IPL%201.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)"    
+  }
 
 }
 
-// TODO: Create a function to generate markdown for README
+// This function generates the markdown for the README
 function generateMarkdown(data) {
   let scribeToF = '## Table of Contents';
 
-  if (data.installation !==''){scribeToF +='*[Installation](#installation)'};
+  if (data.installation !==''){scribeToF += '[Installation](#installation)'};
 
-  if (data.instructions !==''){scribeToF +='*[Instructions](#instructions)'};
+  if (data.instructions !==''){scribeToF += '[Instructions](#instructions)'};
 
-  if (data.contributor !==''){scribeToF +='*[Contributor](#contributor)'};
+  if (data.contributor !==''){scribeToF += '[Contributor](#contributor)'};
 
-  if (data.tests !==''){scribeToF +='*[Tests](#tests)'};
+  if (data.tests !==''){scribeToF += '[Tests](#tests)'};
 
   
   console.log(data)
   
-  return `# ${data.ToC}
+  return `# ${data.scribeToF}
   
-  let freshMarkdown = '# ${data.title}
 
-  ![Badge for GitHub repo TL](https://img.shields.io/github/languages/top/${data.username}/${data.repoName}?style=flat&logo=appveyor), ![Badge for GitHub last commit](https://img.shields.io/github/last-commit/${data.username}/${data.repo}?style=flat&logo=appveyor)
-  https://img.shields.io/github/license/$%7Bdata.username%7D/$%7Bdata.repoName%7D
 
+  ![Badge for GitHub repo TL](https://img.shields.io/github/languages/top/${data.username}/${data.repoName}?style=flat&logo=appveyor), 
+  ![Badge for GitHub last commit](https://img.shields.io/github/last-commit/${data.username}/${data.repo}?style=flat&logo=appveyor)
+  [https://img.shields.io/github/license/${data.username}/${data.repoName}]
+
+  # ${data.title}
+  
+  ## Table of Contents
+  ${data.ToC}
+  
   ## Description
 
   ${data.description}
-
-  
-  ##Table of Contents
-  ${data.ToC}
 
  
   ## Installation
@@ -82,3 +100,4 @@ function generateMarkdown(data) {
 }
 
 module.exports = generateMarkdown
+
